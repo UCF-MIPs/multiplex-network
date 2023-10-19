682675702d51c414433bc3a42e3899a76e0af7fd
Alex-wsl2 <abaekey@knights.ucf.edu>
2023-10-19 11:47:04 -0400

    Added dev-ab branch, adjusted preprocessing file to avoid needing actors file

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,5 @@
+#TODO completely remove 'actors' file, replace with unique 'Source', 'Target' values in TE data
+
 import networkx as nx
 import pandas as pd
 import numpy as np
@@ -11,7 +13,7 @@ pd.set_option('display.max_columns', None)
 edge_types = generate_edge_types.generate_edge_types()
 edge_types = edge_types + ['TM_*', 'TF_*', 'UM_*', 'UF_*', '*_TM', '*_TF', '*_UM', '*_UF']
 
-dataset = 'ukr_v3' # options: skrip_v7, ukr_v3
+dataset = 'skrip_v7' # options: skrip_v7, ukr_v3
 
 # TODO fix? not sure if *_* == total_te
 if dataset=='ukr_v3':
@@ -20,76 +22,57 @@ if dataset=='ukr_v3':
     edge_types = [dict1.get(n,n) for n in edge_types]
 
 # Data
-skrip_v7_te = 'multilayer-network/Data/actor_te_edges_df - Skripal.csv'
-skrip_v7_act = 'multilayer-network/Data/actors_df - Skripal.csv'
-ukr_v3_te = 'multilayer-network/Data/actor_te_edges_df_2022_01_01_2022_05_01 - Ukraine.csv'
-ukr_v3_act = 'multilayer-network/Data/actors_df - Ukraine.csv'
+skrip_v7_te = 'data/Skripal/v7/indv_network/actor_te_edges_df.csv'
+skrip_v7_act = 'data/Skripal/v7/indv_network/actors_df.csv'
+ukr_v3_te = 'data/Ukraine/v3/dynamic/actor_te_edges_df_2022_01_01_2022_05_01.csv'
+ukr_v3_act = 'data/Ukraine/v3/dynamic/indv_actors_df.csv'
 
 te_df_name = f'{dataset}_te'
-act_df_name = f'{dataset}_act'
+#act_df_name = f'{dataset}_act'
 myvars = locals()
 te_df_path = myvars[te_df_name]
-act_df_path = myvars[act_df_name]
+#act_df_path = myvars[act_df_name]
 
-actor_df = pd.read_csv(act_df_path)
-if dataset=='ukr_v3':
-    actors = dict(zip(actor_df.actor_id, actor_df.user_id)) # For just indv users
-elif dataset=='skrip_v7':
-    actors = dict(zip(actor_df.actor_id, actor_df.actor_label)) # For "full_actors" file
+#actor_df = pd.read_csv(act_df_path)
+#if dataset=='ukr_v3':
+#    actors = dict(zip(actor_df.actor_id, actor_df.user_id)) # For just indv users
+#elif dataset=='skrip_v7':
+#    actors = dict(zip(actor_df.actor_id, actor_df.actor_label)) # For "full_actors" file
 
 # Networks
 graph_dict = {}
-
 edge_types2 = ['actors'] + edge_types
 
 out_infl_weights_df = pd.DataFrame(columns = edge_types2)
-out_infl_weights_df['actors'] = actors.values()
+#out_infl_weights_df['actors'] = actors.values()
 #out_infl_weights_df.fillna(value=0, inplace=True)
 
 in_infl_weights_df = pd.DataFrame(columns = edge_types2)
-in_infl_weights_df['actors'] = actors.values()
+#in_infl_weights_df['actors'] = actors.values()
 #in_infl_weights_df.fillna(value=0, inplace=True)
-
- 
-'''
-# Drop rows with all 0's
-out_infl_weights_df = out_infl_weights_df.loc[~( \
-        (out_infl_weights_df['UM_*'] == 0) & \
-        (out_infl_weights_df['UF_*'] == 0) & \
-        (out_infl_weights_df['TM_*'] == 0) & \
-        (out_infl_weights_df['TF_*'] == 0) \
-        )]
-
-in_infl_weights_df = in_infl_weights_df.loc[~( \
-        (in_infl_weights_df['UM_*'] == 0) & \
-        (in_infl_weights_df['UF_*'] == 0) & \
-        (in_infl_weights_df['TM_*'] == 0) & \
-        (in_infl_weights_df['TF_*'] == 0) \
-        )]
-'''
-
-
-
-
 
 # Pre-process #TODO fix to include chunking method
 graph_df = pd.read_csv(te_df_path)
+actors = pd.unique(graph_df[['Source', 'Target']].values.ravel('K'))
+out_infl_weights_df['actors'] = actors
+in_infl_weights_df['actors'] = actors
 
 # Check number of unique users
 num_us = len(pd.unique(graph_df[['Source', 'Target']].values.ravel('K')))
 print(f'number of unique users: {num_us}')
+
 graph_df = add_aggregate_networks.add_aggr_nets(graph_df)
 
 for edge_type in edge_types:
     graph_dict[edge_type] = {}
         
     g = nx.from_pandas_edgelist(graph_df, source='Source', target='Target', edge_attr=[edge_type], create_using=nx.DiGraph())
-    nx.relabel_nodes(g, actors, copy=False)
+    #nx.relabel_nodes(g, actors, copy=False)
 
     graph_dict[edge_type] = g
 
-    # identify which influence types nodes appear in
-    for node in actors.values():
+    # identify which influence types nodes appear in, save summed weights
+    for node in actors:
 
         ## out weight df filling
         if g.has_node(node):
@@ -115,6 +98,6 @@ for edge_type in edge_types:
             row_index = in_infl_weights_df.index[in_infl_weights_df['actors']==node].to_list()
             in_infl_weights_df.loc[row_index, [edge_type]]=summed_weight
 
-out_infl_weights_df.to_csv(f'multilayer-network/Data/preprocessed/{dataset}_out_infl_weights_df.csv')
-in_infl_weights_df.to_csv(f'multilayer-network/Data/preprocessed/{dataset}_in_infl_weights_df.csv')
+out_infl_weights_df.to_csv(f'data/preprocessed/{dataset}_out_infl_weights_df.csv')
+in_infl_weights_df.to_csv(f'data/preprocessed/{dataset}_in_infl_weights_df.csv')
 
