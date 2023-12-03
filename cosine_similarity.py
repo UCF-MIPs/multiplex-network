547b9341fd60b6160c9d2d10b3bddfa896d4f494
Sina Abdidizaji <sinaabdi.d@gmail.com>
2023-12-03 11:33:36 -0500

    adding cosine similarity measures for 2 layers and weighted pairwise comparison

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -39,7 +39,7 @@ for inf in ['TM','TF','UM','UF']:
     elif inf == 'UF':
         for i,j in zip(range(len(dataset_list)), dataset_list):
             combined_dfs[df_name].loc[i] = dataframes[f'df_{j}'].iloc[3]
-    combined_dfs[df_name].to_csv(f'Data/preprocessed/{inf}_cosine_similarity_raw.csv')
+    combined_dfs[df_name].to_csv(f'Data/preprocessed/{inf}_cosine_similarity_raw_weighted.csv')
 
 # cosine similarity for TM
 TM_cosine_similarity_df = pd.DataFrame(index= range(len(dataset_list)), columns = dataset_list)
@@ -50,7 +50,7 @@ for i,j in enumerate(dataset_list):
         TM_cosine_similarity_df.at[j,h] = consine_sim(TM_array[i,:], TM_array[k,:]).round(3)
 TM_cosine_similarity_df.rename(columns=dict_list, inplace=True)
 TM_cosine_similarity_df.index = dict_list.values()
-TM_cosine_similarity_df.to_csv('Data/preprocessed/TM_cosine_similarity.csv')
+TM_cosine_similarity_df.to_csv('results/TM_cosine_similarity_weighted.csv')
 
 
 # cosine similarity for TF
@@ -62,7 +62,7 @@ for i,j in enumerate(dataset_list):
         TF_cosine_similarity_df.at[j,h] = consine_sim(TF_array[i,:], TF_array[k,:]).round(3)
 TF_cosine_similarity_df.rename(columns=dict_list, inplace=True)
 TF_cosine_similarity_df.index = dict_list.values()
-TF_cosine_similarity_df.to_csv('Data/preprocessed/TF_cosine_similarity.csv')
+TF_cosine_similarity_df.to_csv('results/TF_cosine_similarity_weighted.csv')
 
 
 # cosine similarity for UM
@@ -74,7 +74,7 @@ for i,j in enumerate(dataset_list):
         UM_cosine_similarity_df.at[j,h] = consine_sim(UM_array[i,:], UM_array[k,:]).round(3)
 UM_cosine_similarity_df.rename(columns=dict_list, inplace=True)
 UM_cosine_similarity_df.index = dict_list.values()
-UM_cosine_similarity_df.to_csv('Data/preprocessed/UM_cosine_similarity.csv')
+UM_cosine_similarity_df.to_csv('results/UM_cosine_similarity_weighted.csv')
 
 # cosine similarity for UF
 UF_cosine_similarity_df = pd.DataFrame(index= range(len(dataset_list)), columns = dataset_list)
@@ -85,5 +85,46 @@ for i,j in enumerate(dataset_list):
         UF_cosine_similarity_df.at[j,h] = consine_sim(UF_array[i,:], UF_array[k,:]).round(3)
 UF_cosine_similarity_df.rename(columns=dict_list, inplace=True)
 UF_cosine_similarity_df.index = dict_list.values()
-UF_cosine_similarity_df.to_csv('Data/preprocessed/UF_cosine_similarity.csv')
+UF_cosine_similarity_df.to_csv('results/UF_cosine_similarity_weighted.csv')
 
+
+### Cosine similarity for Trustworthy and Untrustworthy ###
+dataframes_TU = {}
+for dataset in dataset_list: 
+    df_name = f'df_{dataset}'
+    df_path = f'Data/preprocessed/{dataset}/{dataset}_quadrant_preprocessing_sources_2layers_weighted.csv'
+    dataframes_TU[df_name] = pd.read_csv(df_path, index_col=0)
+
+combined_dfs_TU = {}
+for inf in ['T','U']:
+    df_name = f'combined_df_{inf}'
+    combined_dfs_TU[df_name] = pd.DataFrame(columns = ['T_*','U_*'])
+    if inf == 'T':
+        for i,j in zip(range(len(dataset_list)), dataset_list):
+            combined_dfs_TU[df_name].loc[i] = dataframes_TU[f'df_{j}'].iloc[0]
+    elif inf == 'U':
+        for i,j in zip(range(len(dataset_list)), dataset_list):
+            combined_dfs_TU[df_name].loc[i] = dataframes_TU[f'df_{j}'].iloc[1]
+    combined_dfs_TU[df_name].to_csv(f'Data/preprocessed/{inf}_cosine_similarity_raw_weighted.csv')
+
+# cosine similarity for Trustworthy
+T_cosine_similarity_df = pd.DataFrame(index= range(len(dataset_list)), columns = dataset_list)
+T_cosine_similarity_df.index = dataset_list
+for i,j in enumerate(dataset_list):
+    for k,h in enumerate(dataset_list):
+        T_array = combined_dfs_TU['combined_df_T'].values
+        T_cosine_similarity_df.at[j,h] = consine_sim(T_array[i,:], T_array[k,:]).round(3)
+T_cosine_similarity_df.rename(columns=dict_list, inplace=True)
+T_cosine_similarity_df.index = dict_list.values()
+T_cosine_similarity_df.to_csv('results/T_cosine_similarity_weighted.csv')
+
+# cosine similarity for Untrustworthy
+U_cosine_similarity_df = pd.DataFrame(index= range(len(dataset_list)), columns = dataset_list)
+U_cosine_similarity_df.index = dataset_list
+for i,j in enumerate(dataset_list):
+    for k,h in enumerate(dataset_list):
+        U_array = combined_dfs_TU['combined_df_U'].values
+        U_cosine_similarity_df.at[j,h] = consine_sim(U_array[i,:], U_array[k,:]).round(3)
+U_cosine_similarity_df.rename(columns=dict_list, inplace=True)
+U_cosine_similarity_df.index = dict_list.values()
+U_cosine_similarity_df.to_csv('results/U_cosine_similarity_weighted.csv')
